e2f2f9517e6a101dbbe1280bbba1ae49ff52f213
Mark Jeschke <markjeschke@yahoo.com>
2024-12-01 10:51:31 -0800

    Initial Commit

---
+++ List Flash/List_FlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_FlashApp.swift
+//  List Flash
+//
+//  Created by Mark Jeschke on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct List_FlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
