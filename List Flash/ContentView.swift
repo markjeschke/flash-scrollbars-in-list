27d04d3ebe23229cd77a35d6423fa99b7c6fbfba
Mark Jeschke <markjeschke@yahoo.com>
2024-12-01 13:14:02 -0800

    first commit

--- List Flash/ContentView.swift
+++ List Flash/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var flashState = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(0..<50, id: \.self) { index in
+                        Text("Row \(index)")
+                            .frame(minHeight: 44)
+                    }
+                }
+                .listStyle(.plain)
+                .scrollIndicatorsFlash(trigger: flashState)
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        flashState.toggle()
+                    }
+                }
+            }
+            .navigationTitle("List Flash")
         }
-        .padding()
     }
 }
 
---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# List Flash
+SwiftUI example, showing how to flash the scrollbars within a List.
+
+## Installation:
+* Within Terminal, cd to your favorite work directory and run `git@github.com:markjeschke/flash-scrollbars-in-list.git`. You can also download the [zip](https://github.com/markjeschke/flash-scrollbars-in-list/archive/refs/heads/main.zip).
+* If you're deploying this app to your physical iOS device, remember to select your developer Team account in the Signing and Capabilities tab in Xcode. Otherwise, you'll receive an error.
+
+### Requirements:
+* Minimum target is iOS 17.6 
+* Updated with Xcode 16.1
